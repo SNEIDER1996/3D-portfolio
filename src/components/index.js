e18af93ad3c98005c1b9a0df4974f8cc761fb73c
sneider obando gomez <99604508+SNEIDER1996@users.noreply.github.com>
2023-11-15 18:33:33 -0500

    final

---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+import Alert from "./Alert";
+import HomeInfo from "./HomeInfo";
+import Loader from "./Loader";
+import Navbar from "./Navbar";
+import CTA from "./CTA";
+
+
+export {
+  Alert,
+  HomeInfo,
+  Loader,
+  Navbar,
+  CTA
+};
---
+++ src/constants/index.js
@@ -0,0 +1,173 @@
+import {
+  car,
+  contact,
+  css,
+  estate,
+  express,
+  git,
+  github,
+  html,
+  javascript,
+  linkedin,
+  mongodb,
+  motion,
+  mui,
+  nextjs,
+  nodejs,
+  pricewise,
+  react,
+  redux,
+  sass,
+  snapgram,
+  summiz,
+  tailwindcss,
+  threads,
+  typescript,
+} from "../assets/icons";
+
+export const skills = [
+  {
+    imageUrl: css,
+    name: "CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: express,
+    name: "Express",
+    type: "Backend",
+  },
+  {
+    imageUrl: git,
+    name: "Git",
+    type: "Version Control",
+  },
+  {
+    imageUrl: github,
+    name: "GitHub",
+    type: "Version Control",
+  },
+  {
+    imageUrl: html,
+    name: "HTML",
+    type: "Frontend",
+  },
+  {
+    imageUrl: javascript,
+    name: "JavaScript",
+    type: "Frontend",
+  },
+  {
+    imageUrl: mongodb,
+    name: "MongoDB",
+    type: "Database",
+  },
+  {
+    imageUrl: motion,
+    name: "Motion",
+    type: "Animation",
+  },
+  {
+    imageUrl: mui,
+    name: "Material-UI",
+    type: "Frontend",
+  },
+  {
+    imageUrl: nextjs,
+    name: "Next.js",
+    type: "Frontend",
+  },
+  {
+    imageUrl: nodejs,
+    name: "Node.js",
+    type: "Backend",
+  },
+  {
+    imageUrl: react,
+    name: "React",
+    type: "Frontend",
+  },
+  {
+    imageUrl: redux,
+    name: "Redux",
+    type: "State Management",
+  },
+  {
+    imageUrl: sass,
+    name: "Sass",
+    type: "Frontend",
+  },
+  {
+    imageUrl: tailwindcss,
+    name: "Tailwind CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: typescript,
+    name: "TypeScript",
+    type: "Frontend",
+  },
+];
+
+export const socialLinks = [
+  {
+    name: "Contact",
+    iconUrl: contact,
+    link: "/contact",
+  },
+  {
+    name: "GitHub",
+    iconUrl: github,
+    link: "https://github.com/YourGitHubUsername",
+  },
+  {
+    name: "LinkedIn",
+    iconUrl: linkedin,
+    link: "https://www.linkedin.com/in/YourLinkedInUsername",
+  },
+];
+
+export const projects = [
+  {
+    iconUrl: pricewise,
+    theme: "btn-back-red",
+    name: "LMS",
+    description: "this is an lms with many of its functionalities",
+    link: "https://github.com/SNEIDER1996/lms",
+  },
+  {
+    iconUrl: threads,
+    theme: "btn-back-green",
+    name: "Ecommerce",
+    description:
+      "this is an ecommerce you can add to cart and has payment method",
+    link: "https://github.com/SNEIDER1996/ecommerce-store",
+  },
+  {
+    iconUrl: car,
+    theme: "btn-back-blue",
+    name: "Movix App",
+    description: "movies and series application",
+    link: "https://github.com/SNEIDER1996/movix",
+  },
+  {
+    iconUrl: snapgram,
+    theme: "btn-back-pink",
+    name: "Messenger clone",
+    description: "this is an messenger clone with many of its functionalities",
+    link: "https://github.com/SNEIDER1996/messenger-clone",
+  },
+  {
+    iconUrl: estate,
+    theme: "btn-back-black",
+    name: "Airbnb Clone",
+    description: "this is an airbnb clone with many of its functionalities",
+    link: "https://github.com/SNEIDER1996/airbnb",
+  },
+  {
+    iconUrl: summiz,
+    theme: "btn-back-yellow",
+    name: "Notion Clone",
+    description: "this is an notion clone with many of its functionalities",
+    link: "https://github.com/SNEIDER1996/notion-clone",
+  },
+];
